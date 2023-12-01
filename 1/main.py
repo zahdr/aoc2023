e12cddbd998228fe054fc46240dcfb90763ba517
zahdr <f.wischendorff@posteo.de>
2023-12-01 12:36:47 +0100

    day 1

---
+++ 1/main.py
@@ -0,0 +1,14 @@
+sum = 0
+
+with open("input.txt") as file:
+    for line in file:
+        number_in_line = ""
+        for character in line:
+            if character.isdigit():
+                number_in_line += character
+        if len(number_in_line) <= 1:
+            sum += int(number_in_line + number_in_line)
+        else:
+            sum +=  int(number_in_line[0] + number_in_line[-1])
+
+print(sum)
